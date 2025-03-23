84ee91d60c3b06db14339ae73b7d8893f1e11e3a
onlyZeref <onurakbas.342@gmail.com>
2025-03-23 10:02:40 +0300

    initial commit

---
+++ src/test/java/com/zeref/zereflix/ZereflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeref.zereflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZereflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
